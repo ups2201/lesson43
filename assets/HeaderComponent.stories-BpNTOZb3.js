0ee7f36e2fb9f0b6ededf8f1f177d6ff681bf28b
ups2201 <74589416+ups2201@users.noreply.github.com>
2024-08-05 06:30:12 +0000

    Deploying to gh-pages from @ ups2201/lesson43@0efebd59c3990e7ca504f1884bf1e3d51b74b18c 🚀

---
+++ assets/HeaderComponent.stories-BpNTOZb3.js
@@ -0,0 +1,31 @@
+import{R as w}from"./index-DJO9vBfz.js";const R=({level:e,text:$})=>{if(e<1||e>6)throw Error(`Не существует тега h${e}`);return w.createElement(`h${e}`,{},$)};R.__docgenInfo={description:"",methods:[],displayName:"HeaderComponent"};const I={component:R},r={args:{level:1,text:"Заголовок 1 уровня"}},s={args:{level:2,text:"Заголовок 2 уровня"}},a={args:{level:3,text:"Заголовок 3 уровня"}},t={args:{level:4,text:"Заголовок 4 уровня"}},o={args:{level:5,text:"Заголовок 5 уровня"}},n={args:{level:6,text:"Заголовок 6 уровня"}};var c,l,m;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    level: 1,
+    text: "Заголовок 1 уровня"
+  }
+}`,...(m=(l=r.parameters)==null?void 0:l.docs)==null?void 0:m.source}}};var p,d,v;s.parameters={...s.parameters,docs:{...(p=s.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    level: 2,
+    text: "Заголовок 2 уровня"
+  }
+}`,...(v=(d=s.parameters)==null?void 0:d.docs)==null?void 0:v.source}}};var u,g,i;a.parameters={...a.parameters,docs:{...(u=a.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    level: 3,
+    text: "Заголовок 3 уровня"
+  }
+}`,...(i=(g=a.parameters)==null?void 0:g.docs)==null?void 0:i.source}}};var x,L,S;t.parameters={...t.parameters,docs:{...(x=t.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    level: 4,
+    text: "Заголовок 4 уровня"
+  }
+}`,...(S=(L=t.parameters)==null?void 0:L.docs)==null?void 0:S.source}}};var f,h,_;o.parameters={...o.parameters,docs:{...(f=o.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    level: 5,
+    text: "Заголовок 5 уровня"
+  }
+}`,...(_=(h=o.parameters)==null?void 0:h.docs)==null?void 0:_.source}}};var E,C,H;n.parameters={...n.parameters,docs:{...(E=n.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  args: {
+    level: 6,
+    text: "Заголовок 6 уровня"
+  }
+}`,...(H=(C=n.parameters)==null?void 0:C.docs)==null?void 0:H.source}}};const N=["Level1","Level2","Level3","Level4","Level5","Level6"];export{r as Level1,s as Level2,a as Level3,t as Level4,o as Level5,n as Level6,N as __namedExportsOrder,I as default};
---
+++ assets/ImageComponent.stories-BaFizBlT.js
@@ -0,0 +1,13 @@
+import{j as s}from"./jsx-runtime-CkxqCPlQ.js";import"./index-DJO9vBfz.js";const m=({type:p,src:g,text:d})=>s.jsxs(s.Fragment,{children:[d,s.jsx("img",{align:p,src:g,className:"imageComponent"})]});m.__docgenInfo={description:"",methods:[],displayName:"ImageComponent"};const u={component:m},e={args:{text:"Текст рядом с изображением",type:"left",src:"./src/stories/image.PNG"}},r={args:{text:"Текст рядом с изображением",level:"right",src:"./src/stories/image.PNG"}};var t,a,o;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    text: "Текст рядом с изображением",
+    type: "left",
+    src: "./src/stories/image.PNG"
+  }
+}`,...(o=(a=e.parameters)==null?void 0:a.docs)==null?void 0:o.source}}};var n,c,i;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    text: "Текст рядом с изображением",
+    level: "right",
+    src: "./src/stories/image.PNG"
+  }
+}`,...(i=(c=r.parameters)==null?void 0:c.docs)==null?void 0:i.source}}};const f=["WrappingLeft","WrappingRight"];export{e as WrappingLeft,r as WrappingRight,f as __namedExportsOrder,u as default};
